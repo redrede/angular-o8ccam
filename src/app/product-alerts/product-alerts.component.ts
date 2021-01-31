9a5cfb6f7418e5866d7165c12374d75d9aca4e6c
redrede <redrede@gmail.com>
2021-01-31 15:55:41 -0300

    2 files modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -9,7 +9,7 @@ import { Output, EventEmitter } from "@angular/core";
 })
 export class ProductAlertsComponent implements OnInit {
   @Input() product;
-  @Output() notify;
+  @Output() notify = new EventEmitter();
   constructor() {}
 
   ngOnInit() {}
